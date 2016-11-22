061fa54a785ac4999ceec41b9889051616c2f9e5
Auron Fang <auron.fang@thinkpower-info.com>
2016-11-22 14:52:29 +0800

    我的第一支iOS App

---
+++ iOS/LoginFlow/LoginViewController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+using UIKit;
+
+using static System.Console;
+
+namespace Auron.iOS
+{
+	public partial class LoginViewController : UIViewController
+	{
+		public LoginViewController(IntPtr handle) : base(handle)
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+
+			btnLogin.TouchUpInside += (sender, e) =>
+			{
+				WriteLine($"TouchUpInside");
+			};
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
---
+++ iOS/WebFlow/MyWebViewController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+using UIKit;
+
+namespace Auron.iOS
+{
+	public partial class MyWebViewController : UIViewController
+	{
+		public MyWebViewController(IntPtr handle) : base(handle)// 這樣就是Storyboard去binding，而不是從xib
+		{
+		}
+
+		public override void ViewDidLoad()
+		{
+			base.ViewDidLoad();
+			// Perform any additional setup after loading the view, typically from a nib.
+		}
+
+		public override void DidReceiveMemoryWarning()
+		{
+			base.DidReceiveMemoryWarning();
+			// Release any cached data, images, etc that aren't in use.
+		}
+	}
+}
+
